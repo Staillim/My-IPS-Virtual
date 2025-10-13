911dc9196d42ea0f8cb11c2fa428e39d1d3b025d
Staillim <harrigta@gmail.com>
2025-10-13 16:27:20 -0500

    Actualización completa del proyecto

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Sidebar } from "@/components/sidebar";
 import { MobileFooter } from "@/components/mobile-footer";
+import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 
 export default function DashboardLayout({
   children,
@@ -7,12 +10,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <main className="flex-1 pb-16 md:pb-0">{children}</main>
-        <MobileFooter />
+    <ProtectedRoute requireAuth={true}>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+          <main className="flex-1 pb-16 md:pb-0">{children}</main>
+          <MobileFooter />
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
